12e1e38f49e011a831ca1419b9cf3644b43e281f
Syed Faraz Ali <128136581+syedfaraazali@users.noreply.github.com>
2024-12-24 18:20:08 +0500

    Update index.php

--- index.php
+++ index.php
@@ -14,12 +14,20 @@
 
 get_header();
 ?>
+    $args = array(
+	'post_type' => 'post',
+	'posts_per_page' => get_option('posts_per_page'),
+	// 'paged' => 1
+);
+$posts = new WP_Query($args);
+
+?>
     <div class="home-main">
 		<div class="row ml-0 mr-0">
 		  <main id="primary" class="site-main col-8">
-			<div class="home-posts">
+			<div id="home-posts-data" class="home-posts">
 				<?php
-					if ( have_posts() ) :
+					if ($posts->have_posts() ) :
 
 					if ( is_home() && ! is_front_page() ) :
 					?>
@@ -30,37 +38,32 @@ get_header();
 						endif;
 
 						/* Start the Loop */
-						while ( have_posts() ) :
-						the_post();
+						while ($posts->have_posts() ) :
+							$posts->the_post();
 
-							/*
-							* Include the Post-Type-specific template for the content.
-							* If you want to override this in a child theme, then include a file
-							* called content-___.php (where ___ is the Post Type name) and that will be used instead.
-							*/
 							get_template_part( 'template-parts/content', get_post_type() );
 
 						endwhile;
 
-						the_posts_navigation();
+						// the_posts_pagination();
 
 						else :
 
-						get_template_part( 'template-parts/content', 'none' );
+						// get_template_part( 'template-parts/content', 'none' );
 
 						endif;
 					?>
 			</div>
+			<button id="load_more" class="btn btn-primary mt-3 mb-3 btn-lg">Load More</button>
 		 </main><!-- #main -->
-		  <div class="home-sidebar col-4">
+		 
+		 <div class="home-sidebar col-4">
 			<?php get_sidebar();?> 
         </div>
 		</div>
 		</div>
 		
 	</div>
-	
-
 <?php
 // get_sidebar();
 get_footer();
